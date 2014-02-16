9f3447954d16cf3a3c3d8d36b98b1b04dcc1c0b3
Nick Babenko <nickbabenko@me.com>
2014-02-16 22:08:00 +0000

    Updated issues to line chart

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Status-Board-Server
 
 Server for serving data as Panic Status Board widgets.
 
-Current widgets:
+Current widge and their required parameters:
 
  - Jira
+  - `host` e.g. company.atlassian.net
+  - `user` - Atlassian account username
+  - `password` - Atlassian account password
  - SickBeard
+  - `host` - will need your public IP if remotely accessed
+  - `api_key` - Given in SickBeard settings
+ - BitBucket
+  - `username` - Bitbucket account username
+  - `token` - Retreivable from dashboard alternative RSS feed
--- action/jira.js
+++ action/jira.js
@@ -2,34 +2,60 @@ var JiraApi = require('jira').JiraApi;
 	
 module.exports = function() {
 	
-	this.app.get('/jira', function(req, res) {
+	this.app.get('/jira', function(req, res) {	
 		var jira = new JiraApi('https', req.query.host, 443, req.query.user, req.query.password, '2', true, false);	
 		
 		jira.listProjects(function(error, projects) {		
 			if(error || typeof projects != 'object')
 				return;
+				
+			var response = {
+				graph: {
+					title: 'Issue Overview',
+					type: 'line',
+					datasequences: []
+				}	
+			};
 		
-			var response 		= '<table id="projects">',
+			var response = {
+					graph: {
+						title: 'Issue Overview',
+						type: 'line',
+						datasequences: []
+					}	
+				},
 				subQueryDone	= 0;
 			
 			projects.forEach(function(project) {
-				jira.searchJira('project = ' + project.key + ' AND status in (Open, "In Progress", Reopened, "To Do")', {}, function(error, openTickets) {				
-					if(error || typeof openTickets != 'object')
-						openTickets = { issues: [] };
-										
-					response += '<tr>';
-					response += '<td class="projectIcon"><img src="' + project.avatarUrls['16x16'] + '" /></td>';
-					response += '<td class="projectName">' + project.name + '</td>';
-					response += '<td class="projectIssues">' + openTickets.issues.length + '</td>';
-					response += '</tr>';
+				jira.searchJira('project = ' + project.key + ' AND status in (Open, "In Progress", Reopened, "To Do", Closed, Resolved)', {}, function(error, issues) {				
+					if(error || typeof issues != 'object') {
+						subQueryDone++;
+						
+						return;
+					}
+											
+					var openTickets = 0,
+						closedTickets = 0;
+												
+					issues.issues.forEach(function(issue) {
+						if(issue.fields.status.name == 'Resolved' || issue.fields.status.name == 'Closed')
+							closedTickets++;
+						else
+							openTickets++;
+					});
+						
+					response.graph.datasequences.push({
+						title: project.name,
+						datapoints: [
+							{ title: "Complete", value: openTickets },
+							{ title: "Incomplete", value: closedTickets }
+						]
+					});
 					
 					subQueryDone++;
 					
-					if(subQueryDone == projects.length - 1) {
-						response += '</table>';
-		
-						res.send(response);
-					}
+					if(subQueryDone == projects.length - 1)
+						res.json(response);
 				});	
 			});
 		});
