6ea0a8e98364dae7c736b04178a2daa295ec18c5
Nick Babenko <nickbabenko@me.com>
2014-02-16 22:57:42 +0000

    Improved sickbeard widget

--- action/sickbeard.js
+++ action/sickbeard.js
@@ -21,22 +21,22 @@ module.exports = function() {
 				var response = '<table id="shows">';
 				
 				if(upcomingEpisodes.data.today.length > 0) {
-					response += '<tr><td colspan="2">TONIGHT</td></tr>';
+					response += '<tr><td colspan="2" style="background: none;">TONIGHT</td></tr>';
 					
 					upcomingEpisodes.data.today.forEach(function(upcomingEpisode) {
 						response += '<tr>';
 						response += '<td class="showName">' + upcomingEpisode.show_name + '</td>';
-						response += '<td class="showEpisode">S' + (upcomingEpisode.season < 9 ? '0' : '') + upcomingEpisode.season + 'E' + (upcomingEpisode.episode < 9 ? '0' : '') + upcomingEpisode.episode + '</td>';
+						response += '<td class="showEpisode" style="width: 30px;">S' + (upcomingEpisode.season < 9 ? '0' : '') + upcomingEpisode.season + 'E' + (upcomingEpisode.episode < 9 ? '0' : '') + upcomingEpisode.episode + '</td>';
 						response += '</tr>';
 					});
 				}
 				
-				response += '<tr><td colspan="2">DOWNLOAD HISTORY</td></tr>';
+				response += '<tr><td colspan="2" style="background: none;">DOWNLOAD HISTORY</td></tr>';
 				
 				pastEpisodes.data.forEach(function(pastEpisode) {
 					response += '<tr>';
 					response += '<td class="showName">' + pastEpisode.show_name + '</td>';
-					response += '<td class="showEpisode">S' + (pastEpisode.season < 9 ? '0' : '') + pastEpisode.season + 'E' + (pastEpisode.episode < 9 ? '0' : '') + pastEpisode.episode + '</td>';
+					response += '<td class="showEpisode" style="width: 30px;">S' + (pastEpisode.season < 9 ? '0' : '') + pastEpisode.season + 'E' + (pastEpisode.episode < 9 ? '0' : '') + pastEpisode.episode + '</td>';
 					response += '</tr>';
 				});
 				
